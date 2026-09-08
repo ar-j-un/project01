7c25eb706f5e5d42ae9d1c1aa213a9eb789e9251
ar-j-un <arjun.menon@prophaze.com>
2026-09-08 12:05:08 +0000

    feat: add modals stack to app layout
    Add @stack('modals') directive to resources/views/layouts/app.blade.php immediately after the main content element to support dynamic modal rendering.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en" class="no-js">
+
 <head>
     <title>{{ $title ?? 'Dashboard' }} | {{ config('app.name', 'Prophaze Dashboard') }}</title>
     <meta charset="utf-8">
@@ -9,10 +10,12 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="//fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('vendor/awesome-dashboard/vendor/themify-icons/themify-icons.css') }}">
-    <link rel="stylesheet" href="{{ asset('vendor/awesome-dashboard/vendor/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css') }}">
+    <link rel="stylesheet"
+        href="{{ asset('vendor/awesome-dashboard/vendor/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css') }}">
     <link rel="stylesheet" href="{{ asset('vendor/awesome-dashboard/css/theme.css') }}">
     @stack('styles')
 </head>
+
 <body>
     @include('partials.topbar')
     <main class="u-main">
@@ -30,15 +33,19 @@
             @include('partials.footer')
         </div>
     </main>
+    @stack('modals')
     <script src="{{ asset('vendor/awesome-dashboard/vendor/jquery/dist/jquery.min.js') }}"></script>
     <script src="{{ asset('vendor/awesome-dashboard/vendor/jquery-migrate/jquery-migrate.min.js') }}"></script>
     <script src="{{ asset('vendor/awesome-dashboard/vendor/popper.js/dist/umd/popper.min.js') }}"></script>
     <script src="{{ asset('vendor/awesome-dashboard/vendor/bootstrap/dist/js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('vendor/awesome-dashboard/vendor/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min.js') }}"></script>
+    <script
+        src="{{ asset('vendor/awesome-dashboard/vendor/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min.js') }}"></script>
     <script src="{{ asset('vendor/awesome-dashboard/vendor/chart.js/dist/Chart.min.js') }}"></script>
-    <script src="{{ asset('vendor/awesome-dashboard/vendor/chartjs-plugin-style/dist/chartjs-plugin-style.min.js') }}"></script>
+    <script
+        src="{{ asset('vendor/awesome-dashboard/vendor/chartjs-plugin-style/dist/chartjs-plugin-style.min.js') }}"></script>
     <script src="{{ asset('vendor/awesome-dashboard/js/sidebar-nav.js') }}"></script>
     <script src="{{ asset('vendor/awesome-dashboard/js/main.js') }}"></script>
     @stack('scripts')
 </body>
+
 </html>
